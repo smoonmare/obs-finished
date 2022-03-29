10558341a6192391ef7b42a043bc5e0be80e1026
smoonmare <smoonmare@gmail.com>
2022-03-28 22:11:38 -0700

    Created custom interval Observable.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,22 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
-import { interval } from 'rxjs';
+import { interval, Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  private firstObsSubscription?: Subscription;
 
   constructor() { }
 
   ngOnInit(): void {
-    interval(1000).subscribe(
-      count => {
-        console.log(count);
-      }
-    );
+    // this.firstObsSubscription = interval(1000).subscribe(
+    //   count => {
+    //     console.log(count);
+    //   }
+    // );
+    const customIntervalObservable = new Observable(observer => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        count++;
+      }, 1000);
+    });
+    this.firstObsSubscription = customIntervalObservable.subscribe(data => {
+      console.log(data);
+    });
+  }
+
+  ngOnDestroy(): void {
+      this.firstObsSubscription?.unsubscribe();
   }
 
 }
